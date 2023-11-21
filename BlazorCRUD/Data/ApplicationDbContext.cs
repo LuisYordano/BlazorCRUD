893867da076615b6f1bf40155b5ff950008287e0
Luis Yordano <lycruzc@yahoo.com>
2023-11-21 11:30:14 -0500

    Add project files.

---
+++ BlazorCRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BlazorCRUD;
+
+namespace BlazorCRUD.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<BlazorCRUD.Robot> Robot { get; set; } = default!;
+    }
+}
---
+++ BlazorCRUD/Robot.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorCRUD
+{
+    public class Robot
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Company { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? ReleaseDate { get; set; }
+    }
+}
